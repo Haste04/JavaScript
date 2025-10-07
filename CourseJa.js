65031cd488b617ad90914752babf0bd8ea3c177e
Haggai <haggaiestavilla04@gmail.com>
2025-10-07 21:17:06 +0800

    Added Save Functionality

--- CourseJa.js
+++ CourseJa.js
@@ -1,16 +1,21 @@
 let welcomeEl = document.getElementById("welcome-el")
-let name = "User"
+let name = "User! "
 let greeting = "Welcome, "
-welcomeEl.innerText = greeting + name
+welcomeEl.innerText = greeting + name + "👋"
 
 let countEl = document.getElementById("count-el")
-let count = -1
+let count = 0
+
+let previousEntries = document.getElementById("previous-entries")
 
 function increment() {
-    count +=1
-    countEl.innerText = count + 1
+    count += 1
+    countEl.innerText = count
 }
 
 function save(){
-    console.log(count)
+    let entries = " " + count + " - ";
+    previousEntries.innerText += entries
+    count = 0;
+    countEl.innerText = count
 }
