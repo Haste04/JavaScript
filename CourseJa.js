073d66c1683fd910163bb39bea32b90487faea20
Haggai <haggaiestavilla04@gmail.com>
2025-10-09 22:08:57 +0800

    Added Save Minitab and its functionalities, Added Profit

--- CourseJa.js
+++ CourseJa.js
@@ -1,3 +1,4 @@
+//Elements
 let welcomeEl = document.getElementById("welcome-el")
 let name = "User! "
 let greeting = "Welcome, "
@@ -8,14 +9,46 @@ let count = 0
 
 let previousEntries = document.getElementById("previous-entries")
 
+let profitEntries = document.getElementById("profit-entries")
+
+const saveTab = document.getElementById("save-tab");
+const yesButton = document.getElementById("yes-btn");
+const noButton = document.getElementById("no-btn");
+
+//Buttons
+let saveButtonCount = 0;
+const saveBtn = document.getElementById("save-btn")
+
+saveBtn.addEventListener("click", function(){
+    saveTab.classList.remove("hidden");
+})
+
+yesButton.addEventListener("click", function(){
+    saveTab.classList.add("hidden");
+})
+
+noButton.addEventListener("click", function(){
+    saveTab.classList.add("hidden");
+})
+
+//Functions for the buttons
 function increment() {
     count += 1
     countEl.textContent = count
 }
 
-function save(){
+function saveYes(){
+    if (saveButtonCount === 7){
+        previousEntries.textContent = "Previous Entries: "
+        profitEntries.textContent = "Profit: "
+        saveButtonCount = 0;
+    }
+
     let entries = count + " - ";
     previousEntries.textContent += entries
+
+    let profit = "P" + count * 15 + " - ";
+    profitEntries.textContent += profit
     count = 0;
     countEl.textContent = count
 }
